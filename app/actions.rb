85040fe802f7e3a5eb90aed174d99cce240e69bd
stevenpslade <stevenpslade@gmail.com>
2015-10-19 20:32:45 +0000

    form adds content to songs page

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,18 @@ get '/songs' do
   @songs= Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  @song.save
+  redirect '/songs'
+end
